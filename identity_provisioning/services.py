5490107df9fac4de76a2a036b2473f96d7d6039c
Karol Szuster <karol.szuster@upsidelab.io>
2023-03-27 16:56:56 +0200

    Add tools

--- identity_provisioning/services.py
+++ identity_provisioning/services.py
@@ -5,54 +5,23 @@ import config
 import entities
 import schemas
 import exceptions
-from lib import datastore, secret_manager, google_workspace
+from tools import datastore, secret_manager, google_workspace
 
 
-def provision_cloud_identity(cloud_identity: entities.CloudIdentity):
+def provision_cloud_identity(cloud_identity: entities.CloudIdentity) -> entities.CloudIdentity:
     try:
         _persist_cloud_identity(cloud_identity)
     except exceptions.CloudIdentityAlreadyExistsError:
-        print("Cloud Identity alread in Datastoer")
         pass
 
     try:
         _create_cloud_identity_in_google_workspace(cloud_identity)
     except exceptions.GoogleWorkspaceUserAlreadyExistsError:
-        print("Yser Exists")
         pass
 
     _allow_to_create_billing_accounts(cloud_identity)
 
     return cloud_identity
-
-
-def _create_cloud_identity_in_google_workspace(cloud_identity: entities.CloudIdentity):
-    service_account_secret = json.loads(
-        secret_manager.fetch_secret(config.PROJECT_ID, "cloud-identity-secret", 1)
-    )
-    google_admin_credentials = google_workspace.build_service_account_credentials(
-        service_account_secret
-    )
-    google_workspace_user = entities.GoogleWorkspaceUser.from_cloud_identity(
-        cloud_identity
-    )
-    serialized_google_workspace_user = schemas.GoogleWorkspaceUser().dump(
-        google_workspace_user
-    )
-    try:
-        google_workspace.create_user(
-            google_admin_credentials, serialized_google_workspace_user
-        )
-    except google_workspace.UserAlreadyExistsError:
-        raise exceptions.GoogleWorkspaceUserAlreadyExistsError
-
-    return cloud_identity
-
-
-def _allow_to_create_billing_accounts(cloud_identity: entities.CloudIdentity):
-    google_workspace.add_user_to_group(
-        cloud_identity.email, config.BILLING_ACCOUNT_CREATOR_GROUP_ID
-    )
 
 
 def _persist_cloud_identity(cloud_identity: entities.CloudIdentity):
@@ -81,3 +50,24 @@ def _fetch_persisted_cloud_identity(
         return None
 
     return schemas.CloudIdentity().load(query_result[0])
+
+
+def _create_cloud_identity_in_google_workspace(cloud_identity: entities.CloudIdentity):
+    google_workspace_user = entities.GoogleWorkspaceUser.from_cloud_identity(
+        cloud_identity
+    )
+    serialized_google_workspace_user = schemas.GoogleWorkspaceUser().dump(
+        google_workspace_user
+    )
+    try:
+        google_workspace.create_user(serialized_google_workspace_user)
+    except google_workspace.UserAlreadyExistsError:
+        raise exceptions.GoogleWorkspaceUserAlreadyExistsError
+
+    return cloud_identity
+
+
+def _allow_to_create_billing_accounts(cloud_identity: entities.CloudIdentity):
+    google_workspace.add_user_to_group(
+        cloud_identity.email, config.BILLING_ACCOUNT_CREATOR_GROUP_ID
+    )
---
+++ identity_provisioning/tools/google_workspace.py
@@ -0,0 +1,29 @@
+import google.auth
+from googleapiclient import errors
+from googleapiclient.discovery import build
+
+
+class UserAlreadyExistsError(Exception):
+    pass
+
+
+def create_user(body: dict) -> dict:
+    admin_service = build("admin", "directory_v1", client_options={"scopes": ["https://www.googleapis.com/auth/admin.directory.user"]})
+    try:
+        created_user = admin_service.users().insert(body=body).execute()
+    except errors.HttpError as error:
+        if error.status_code == 409:
+            raise UserAlreadyExistsError
+        raise error
+
+    return created_user
+
+
+def add_user_to_group(user_email: str, group_id: str) -> dict:
+    cloud_identity_service = build("cloudidentity", "v1")
+    body = {"preferredMemberKey": {"id": user_email}, "roles": {"name": "MEMBER"}}
+    group_id = f"groups/{group_id}"
+    created_policy = (
+        cloud_identity_service.groups().memberships().create(parent=group_id, body=body).execute()
+    )
+    return created_policy
