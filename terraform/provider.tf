e3c64c86f38bc52c27befff7d4ef944106c74daa
Karol Szuster <karol.szuster@upsidelab.io>
2023-03-28 12:53:16 +0200

    Store Terraform state in a GCS bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,10 @@ provider "google" {
   credentials = file("./credentials.json")
   project     = var.project_id
 }
+
+terraform {
+  backend "gcs" {
+    credentials = "./credentials.json"
+    bucket      = "healthdatanexus-research-environment-api-terraform-state"
+  }
+}
