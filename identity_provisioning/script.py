0162552514bdd3f55550f22ea05a9beb9a89185b
Karol Szuster <karol.szuster@upsidelab.io>
2023-03-28 11:09:42 +0200

    Delete testing script

--- identity_provisioning/script.py
+++
@@ -1,9 +0,0 @@
-import schemas
-import services
-
-user = {"userName": "karol.szuster", "familyName": "Szuster", "givenName": "Karol"}
-cloud_identity = schemas.CloudIdentityCreation().load(user)
-
-# cloud_identity = services._persist_cloud_identity(cloud_identity)
-cloud_identity = services.provision_cloud_identity(cloud_identity)
-print(cloud_identity)
