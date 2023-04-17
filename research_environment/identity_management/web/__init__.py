7e0e074c366888f162b44d9d80cb39512e243fbc
Karol Szuster <karol.szuster@upsidelab.io>
2023-04-17 12:17:36 +0200

    Rename `controllers.py` to `views.py`

--- research_environment/identity_management/web/__init__.py
+++ research_environment/identity_management/web/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 identity_management_bp = Blueprint("identity_management", __name__)
 
-import research_environment.identity_management.web.controllers
+import research_environment.identity_management.web.views
