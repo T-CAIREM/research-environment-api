319cd8a25a74d0a910d00b65fd1cfaad85d47bbb
Karol Szuster <karol.szuster@upsidelab.io>
2023-04-18 12:53:41 +0200

    Add an abstract `BaseModel`

--- research_environment_api/modules/identity_management/models.py
+++ research_environment_api/modules/identity_management/models.py
@@ -1,14 +1,6 @@
-import sqlalchemy
-from sqlalchemy.dialects.postgresql import UUID
-
-from research_environment_api.modules.model import db
+from research_environment_api.modules.model import BaseModel
 
 
-class CloudIdentity(db.Model):
-    id = db.Column(
-        UUID(as_uuid=True),
-        primary_key=True,
-        server_default=sqlalchemy.text("gen_random_uuid()"),
-    )
+class CloudIdentity(BaseModel):
     primary_email = db.Column(db.String(256), unique=True)
     is_configured = db.Column(db.Boolean(), default=False)
--- research_environment_api/modules/model.py
+++ research_environment_api/modules/model.py
@@ -1,6 +1,18 @@
+import sqlalchemy
+from sqlalchemy.dialects.postgresql import UUID
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
 migrate = Migrate(db=db)
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=sqlalchemy.text("gen_random_uuid()"),
+    )
