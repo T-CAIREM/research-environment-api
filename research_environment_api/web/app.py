8c41ffc98c32e576a276014924431660d936cc9a
Karol Szuster <karol.szuster@upsidelab.io>
2023-04-18 09:49:15 +0200

    Initialize `flask_migrate` with the app and database

--- research_environment_api/web/app.py
+++ research_environment_api/web/app.py
@@ -7,9 +7,10 @@ def create_app(config_object: str):
     app = Flask(__name__)
     app.config.from_object(config_object)
 
-    from research_environment_api.modules.model import db
+    from research_environment_api.modules.model import db, migrate
 
     db.init_app(app)
+    migrate.init_app(app)
 
     app.register_blueprint(identity_management_bp)
 
