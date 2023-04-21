ce7e1ec35bf975d2b38ea937aa453d66b3a47bc2
Karol Szuster <karol.szuster@upsidelab.io>
2023-04-21 14:53:16 +0200

    Add `with_delegated_client` domain-wide delegation decorator

--- research_environment_api/library/google/auth.py
+++
@@ -1,7 +0,0 @@
-import google.auth
-
-
-def create_delegated_credentials_for(
-    user_email: str, credentials: google.auth.Credentials = google.auth.default()
-) -> google.auth.Credentials:
-    return credentials.with_subject(user_email)
--- research_environment_api/library/google/delegation.py
+++ research_environment_api/library/google/delegation.py
@@ -1,10 +1,10 @@
-from typing import Callable, List
+from typing import Callable, List, Type
 from functools import wraps
 
 from google.oauth2 import service_account
 
 
-def delegated(scopes: List[str]):
+def with_delegated_client(client_class: Type, scopes: List[str]):
     def wrapper(function: Callable):
         @wraps(function)
         def inner(
@@ -12,7 +12,8 @@ def delegated(scopes: List[str]):
         ):
             scoped_credentials = credentials.with_scopes(scopes)
             delegated_credentials = scoped_credentials.with_subject(user_email)
-            return function(delegated_credentials, *args, **kwargs)
+            client = client_class(credentials=delegated_credentials)
+            return function(client, *args, **kwargs)
 
         return inner
 
