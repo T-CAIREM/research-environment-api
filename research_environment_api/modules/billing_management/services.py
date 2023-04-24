c9b196563ddf2d99d0bdc3423dd59ad618d90af5
Karol Szuster <kszuster@protonmail.com>
2023-04-24 10:58:29 +0200

    Format code with `black`

--- research_environment_api/modules/billing_management/services.py
+++ research_environment_api/modules/billing_management/services.py
@@ -3,5 +3,7 @@ import google.oauth2.service_account as service_account
 import research_environment_api.library.google.billing as google_billing_api
 
 
-def list_billing_accounts_for(user_email: str, credentials: service_account.Credentials):
+def list_billing_accounts_for(
+    user_email: str, credentials: service_account.Credentials
+):
     return google_billing_api.list_billing_accounts(user_email, credentials)
