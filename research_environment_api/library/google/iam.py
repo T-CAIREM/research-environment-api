27846fa1468677f6f64f8fe1da934d9b61bb2cf9
Karol Szuster <kszuster@protonmail.com>
2023-04-28 17:55:55 +0200

    Add `iam` library for listing policies

---
+++ research_environment_api/library/google/iam.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+from google.cloud import asset
+from google.oauth2 import service_account
+
+
+def list_organization_policies(
+    email: str,
+    credentials: service_account.Credentials,
+    organization_id: str,
+    resource: str,
+):
+    client = asset.AssetServiceClient(credentials=credentials)
+    scope = f"organizations/{organization_id}"
+    query = f"{resource} policy: {email}"
+
+    return client.search_all_iam_policies(request={"scope": scope, "query": query})
+
+
+def assign_policy(email: str, policy: str, resource: Optional[str] = None):
+    pass
