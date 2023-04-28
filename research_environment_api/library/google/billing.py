22a76aefa33964247c3b3848cd14dbc92d640388
Karol Szuster <kszuster@protonmail.com>
2023-04-28 17:56:47 +0200

    Add `billing` library for listing billing accounts

--- research_environment_api/library/google/billing.py
+++ research_environment_api/library/google/billing.py
@@ -1,12 +1,36 @@
+from functools import partial
+
 from google.oauth2 import service_account
-from google.cloud.billing import CloudBillingClient, BillingAccount
 
-from research_environment_api.library.google.delegation import with_delegated_client
+from research_environment_api.library.google import iam
 
 
-@with_delegated_client(
-    CloudBillingClient,
-    scopes=["https://www.googleapis.com/auth/cloud-billing.readonly"],
+def list_billing_accounts_by_role(
+    role: str,
+    credentials: service_account.Credentials,
+    email: str,
+    organization_id: str,
+):
+    results = iam.list_organization_policies(
+        email,
+        credentials,
+        organization_id,
+        "resource://cloudbilling.googleapis.com/billingAccounts",
+    )
+
+    billing_accounts_with_role = [
+        policy_result.resource
+        for policy_result in results
+        if any(binding.role == role for binding in policy_result.policy.bindings)
+    ]
+    return billing_accounts_with_role
+
+
+list_owned_billing_accounts = partial(
+    list_billing_accounts_by_role, "roles/billing.admin"
 )
-def list_billing_accounts(client: CloudBillingClient):
-    return client.list_billing_accounts()
+
+
+list_shared_billing_accounts = partial(
+    list_billing_accounts_by_role, "roles/billing.user"
+)
