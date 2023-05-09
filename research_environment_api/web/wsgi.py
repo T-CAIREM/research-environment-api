ed50476b66525871db5f8646ae9259f98d8d0e6d
Karol Szuster <kszuster@protonmail.com>
2023-05-09 11:46:26 +0200

    Add `python-dotenv`

--- research_environment_api/web/wsgi.py
+++ research_environment_api/web/wsgi.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+
 from research_environment_api.web.app import create_app
+
+# FIXME: Move to a more sensible place one a production config is created.
+load_dotenv()
 
 app = create_app("research_environment_api.config.DevelopmentConfig")
