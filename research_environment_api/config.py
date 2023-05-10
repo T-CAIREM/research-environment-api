d548a840418a14bb39c901ea29d5bd1156402319
Karol Szuster <kszuster@protonmail.com>
2023-05-10 09:53:49 +0200

    Move database URI configuration variable into the base `Config` class

--- research_environment_api/config.py
+++ research_environment_api/config.py
@@ -6,6 +6,7 @@ from google.oauth2 import service_account
 class Config(object):
     """Base config"""
 
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
     PROJECT_ID = os.environ["PROJECT_ID"]
     BILLING_ACCOUNT_CREATOR_GROUP_ID = os.environ["BILLING_ACCOUNT_CREATOR_GROUP_ID"]
     SERVICE_ACCOUNT_CREDENTIALS = service_account.Credentials.from_service_account_file(
@@ -18,6 +19,6 @@ class Config(object):
 class DevelopmentConfig(Config):
     """Development config"""
 
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql://postgres:password@localhost:5432/research_environment_api_dev"
-    )
+
+class ProductionConfig(Config):
+    """Production config"""
