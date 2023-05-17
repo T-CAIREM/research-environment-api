64dc4a0d0efe66e635f763b4d2275108340f7f99
Karol Szuster <kszuster@protonmail.com>
2023-05-17 13:14:55 +0200

    Return `409` instead of `422` upon CloudIdentity conflicts

--- research_environment_api/web/identity_management/error_handlers.py
+++ research_environment_api/web/identity_management/error_handlers.py
@@ -11,4 +11,4 @@ def handle_validation_error(error):
 
 @identity_management_bp.errorhandler(exceptions.CloudIdentityAlreadyConfiguredError)
 def handle_validation_error(error):
-    return str(error), 422
+    return str(error), 409
