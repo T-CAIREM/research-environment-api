a570db647a7c21b732b808555146155466604eb8
Rafal Kosla <rafalkosla101@gmail.com>
2023-05-25 14:36:08 +0200

    Change europe enum to proper europe3

--- research_environment_api/modules/workspace_management/enums.py
+++ research_environment_api/modules/workspace_management/enums.py
@@ -4,5 +4,5 @@ from enum import Enum
 class Region(Enum):
     US_CENTRAL = "us-central1"
     NORTHAMERICA_NORTHEAST = "northamerica-northeast1"
-    EUROPE_WEST = "europe-west1"
+    EUROPE_WEST = "europe-west3"
     AUSTRALIA_SOUTHEAST = "australia-southeast1"
