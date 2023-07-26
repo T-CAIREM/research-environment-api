1468653a4f7d39f8e76acec21387f7b36ec17514
Rafal Kosla <rafalkosla101@gmail.com>
2023-07-26 13:41:49 +0200

    Adjust workbench schemas

--- research_environment_api/web/workbench_management/schemas.py
+++ research_environment_api/web/workbench_management/schemas.py
@@ -1,8 +1,12 @@
 from marshmallow import Schema, fields, validate
 
 
-class WorkbenchCreationRequest(Schema):
+class WorkbenchBaseClass(Schema):
     workbench_type = fields.Str(required=True)
+    workspace_project_id = fields.Str(required=True)
+
+
+class WorkbenchCreationRequest(WorkbenchBaseClass):
     machine_type = fields.Str(required=True)
     workspace_project_id = fields.Str(required=True)
     dataset_identifier = fields.Str(required=True)
@@ -13,22 +17,11 @@ class WorkbenchCreationRequest(Schema):
     gpu_accelerator_type = fields.Str()
 
 
-class WorkbenchStopRequest(Schema):
-    workbench_type = fields.Str(required=True)
+class WorkbenchStopRequest(WorkbenchBaseClass):
     user_email = fields.Str(required=True)
-    workspace_project_id = fields.Str(requried=True)
     workbench_resource_id = fields.Str(required=True)
     zone = fields.Str()
 
 
-class WorkbenchUpdateRequest(Schema):
-    workbench_type = fields.Str(required=True)
-    machine_type = fields.Str(required=True)
+class WorkbenchUpdateRequest(WorkbenchCreationRequest):
     workbench_resource_id = fields.Str(required=True)
-    workspace_project_id = fields.Str(requried=True)
-    dataset_identifier = fields.Str(required=True)
-    user_email = fields.Str(required=True)
-    bucket_name = fields.Str(required=True)
-    region = fields.Str(required=True)
-    persistent_disk = fields.Str(required=True)
-    gpu_accelerator_type = fields.Str(required=True)
