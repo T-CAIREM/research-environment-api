c192c53408f98c2d402951829c0f4248dbd2ab12
Karol Szuster <kszuster@protonmail.com>
2023-08-07 19:34:18 +0200

    Specify all environment variables in Terraform

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -34,14 +34,26 @@ resource "google_cloud_run_service" "api" {
           name       = local.service_account_credentials_volume_name
           mount_path = "/app/core_service_account_secret"
         }
+
+        # HACK: Use a different serializer. Pickle requires this to be set in order to work. Bad idea.
+        env {
+          name  = "C_FORCE_ROOT"
+          value = "True"
+        }
+
         env {
           name  = "APP_ENV"
           value = "production"
         }
 
         env {
-          name  = "DATABASE_NAME"
-          value = var.database_name
+          name  = "SERVICE_ACCOUNT_CREDENTIALS_PATH"
+          value = "/app/core_service_account_secret/${var.service_account_credentials_secret_name}"
+        }
+
+        env {
+          name  = "CLOUD_BUILD_SERVICE_ACCOUNT_NAME"
+          value = var.cloud_build_service_account_name
         }
 
         env {
@@ -50,22 +62,69 @@ resource "google_cloud_run_service" "api" {
         }
 
         env {
+          name  = "DATABASE_NAME"
+          value = var.database_name
+        }
+
+        env {
+          name  = "DATABASE_USER"
+          value = var.service_account_name
+        }
+
+        env {
+          name  = "CLOUD_SQL_INSTANCE_CONNECTION_NAME"
+          value = var.cloud_sql_instance_connection_name
+        }
+
+        env {
+          name  = "CELERY_RESULT_BACKEND"
+          value = var.celery_backend_url
+        }
+
+        env {
+          name  = "CELERY_BROKER_URL"
+          value = var.celery_broker_url
+        }
+
+        env {
+          name  = "CACHE_TYPE"
+          value = var.cache_type
+        }
+
+        env {
           name  = "BILLING_ACCOUNT_CREATOR_GROUP_ID"
           value = var.billing_account_creator_group_id
         }
 
         env {
-          name  = "SERVICE_ACCOUNT_CREDENTIALS_PATH"
-          value = "/app/core_service_account_secret/core-service-account-key"
+          name  = "VPC_SECURE_PERIMETER_NAME"
+          value = var.vpc_secure_perimeter_name
+        }
+
+        env {
+          name  = "TERRAFORM_REPO_NAME"
+          value = var.terraform_repo_name
+        }
+
+        env {
+          name  = "TERRAFORM_BRANCH_NAME"
+          value = var.terraform_branch_name
+        }
+
+        env {
+          name  = "JUPYTER_STARTUP_SCRIPT"
+          value = var.jupyter_startup_script
         }
       }
+
       service_account_name = var.service_account_name
     }
 
     metadata {
       annotations = {
-        "autoscaling.knative.dev/minScale" = var.min_scale
-        "autoscaling.knative.dev/maxScale" = var.max_scale
+        "autoscaling.knative.dev/minScale"        = var.min_scale
+        "autoscaling.knative.dev/maxScale"        = var.max_scale
+        "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
       }
     }
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,6 +15,10 @@ variable "service_account_name" {
   type = string
 }
 
+variable "cloud_build_service_account_name" {
+  type = string
+}
+
 variable "image_repository" {
   type = string
 }
@@ -24,6 +28,38 @@ variable "image_tag" {
 }
 
 variable "database_name" {
+  type = string
+}
+
+variable "cloud_sql_instance_connection_name" {
+  type = string
+}
+
+variable "celery_broker_url" {
+  type = string
+}
+
+variable "celery_backend_url" {
+  type = string
+}
+
+variable "cache_type" {
+  type = string
+}
+
+variable "terraform_repo_name" {
+  type = string
+}
+
+variable "terraform_branch_name" {
+  type = string
+}
+
+variable "vpc_secure_perimeter_name" {
+  type = string
+}
+
+variable "jupyter_startup_script" {
   type = string
 }
 
@@ -39,22 +75,10 @@ variable "db_tier" {
   type = string
 }
 
-variable "db_password_secret_name" {
-  type = string
-}
-
 variable "billing_account_creator_group_id" {
   type = string
 }
 
 variable "service_account_credentials_secret_name" {
-  type = string
-}
-
-variable "celery_broker_url" {
-  type = string
-}
-
-variable "celery_backend_url" {
   type = string
 }
