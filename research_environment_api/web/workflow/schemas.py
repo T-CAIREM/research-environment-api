0946ffc86f01690d478befdf1dbbf7c9cd84bdc9
Rafal Kosla <rafalkosla101@gmail.com>
2023-08-18 14:32:56 +0200

    Fix passing attributes to schemas

--- research_environment_api/web/workflow/schemas.py
+++ research_environment_api/web/workflow/schemas.py
@@ -5,5 +5,5 @@ from research_environment_api.background.enums import BuildType, WorkflowStatus
 class Workflow(Schema):
     id = fields.Str(required=True)
     build_type = fields.Enum(BuildType, by_value=True, required=True)
-    status = fields.Enum(WorkflowStatus, by_value=True, required=True)
-    error = fields.Str(required=False)
+    status = fields.Enum(WorkflowStatus, by_value=True, required=True, attribute="build_status")
+    error = fields.Str(required=False, attribute="build_error_information")
