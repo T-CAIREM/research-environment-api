31eec0a5dde126c4eef446fc20aff7f9f1eb5b7b
Rafal Kosla <rafalkosla101@gmail.com>
2023-08-23 11:24:13 +0200

    Add workflows in progress endpoint

--- research_environment_api/web/workflow/views.py
+++ research_environment_api/web/workflow/views.py
@@ -10,3 +10,10 @@ def get_workflow(workflow_id: str):
     workflow = monitoring.get_workflow(workflow_id)
     serialized_workflow = schemas.Workflow().dump(workflow)
     return serialized_workflow, 200
+
+
+@workflow_bp.get("/list/<user_email>")
+def list_workflows(user_email):
+    workflows_list = monitoring.list_active_workflows(user_email)
+    serialized_workflows = schemas.Workflow(many=True).dump(workflows_list)
+    return serialized_workflows, 200
