9ae4307462958effb4a06f3fa8057d9749787aa4
Karol Szuster <kszuster@protonmail.com>
2023-08-25 10:09:05 +0200

    Add missing doc for `/workflows/<workflow_id>`

--- research_environment_api/web/workflow/views.py
+++ research_environment_api/web/workflow/views.py
@@ -7,6 +7,24 @@ from research_environment_api.web.workflow import schemas
 
 @workflow_bp.get("/<workflow_id>")
 def get_workflow(workflow_id: str):
+    """Fetches the specified workflow.
+    ---
+    put:
+      tags:
+        - workflows
+      description: Fetches the specified workflow.
+      parameters:
+      - in: path
+        name: workflow_id
+        schema:
+          type: string
+      responses:
+        200:
+          description: Returns the workflow.
+          content:
+            application/json:
+              schema: Workflow
+    """
     workflow = monitoring.get_workflow(workflow_id)
     serialized_workflow = schemas.Workflow().dump(workflow)
     return serialized_workflow, 200
