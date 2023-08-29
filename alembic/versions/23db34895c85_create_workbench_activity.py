1a0c077a49005eb7a0347697696d1a1cfc992855
Karol Szuster <kszuster@protonmail.com>
2023-08-29 17:06:23 +0200

    Reorder migration imports

--- alembic/versions/23db34895c85_create_workbench_activity.py
+++ alembic/versions/23db34895c85_create_workbench_activity.py
@@ -5,9 +5,9 @@ Revises:
 Create Date: 2023-08-28 08:54:24.755130
 
 """
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "23db34895c85"
