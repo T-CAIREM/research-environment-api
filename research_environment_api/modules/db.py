8de856f1803ce8c2eed6e3f85ae4c41ad487166d
Karol Szuster <kszuster@protonmail.com>
2023-09-05 13:54:39 +0200

    Synchronize Cloud SQL Connector token refresh
    The process of refreshing the token is not thread-safe and leads to
    errors in SSL exchanges (due to it using a urllib3 HTTP pool).

--- research_environment_api/modules/db.py
+++ research_environment_api/modules/db.py
@@ -1,22 +1,11 @@
-import google.auth.transport.requests
+import threading
+
 import pg8000
 import sqlalchemy
 from google.auth.credentials import Credentials
 from google.cloud.sql.connector import Connector, IPTypes
 
-
-def _generate_credentials_token(credentials: Credentials) -> str:
-    cloud_sql_scopes = ["https://www.googleapis.com/auth/sqlservice.admin"]
-    scoped_credentials = credentials.with_scopes(cloud_sql_scopes)
-
-    request = google.auth.transport.requests.Request()
-    scoped_credentials.refresh(request)
-
-    return scoped_credentials.token
-
-
-def _generate_credentials_user(credentials: Credentials) -> str:
-    return credentials.service_account_email.removesuffix(".gserviceaccount.com")
+lock = threading.Lock()
 
 
 def create_cloud_sql_engine(
@@ -27,21 +16,24 @@ def create_cloud_sql_engine(
     def getconn() -> pg8000.dbapi.Connection:
         # The connector needs to be inside of `getconn` to work with Celery workers/threads.
         # Otherwise, the connector is not initialized properly and tasks hang on the connection indefinitely.
-        connector = Connector(credentials=service_account_credentials)
-        iam_service_account_user = _generate_credentials_user(
-            service_account_credentials
+        connector = Connector(
+            credentials=service_account_credentials, enable_iam_auth=True
         )
-        iam_service_account_password = _generate_credentials_token(
-            service_account_credentials
+        iam_service_account_user = (
+            service_account_credentials.service_account_email.removesuffix(
+                ".gserviceaccount.com"
+            )
         )
-        conn: pg8000.dbapi.Connection = connector.connect(
-            instance_connection_name,
-            "pg8000",
-            user=iam_service_account_user,
-            password=iam_service_account_password,
-            db=database_name,
-            ip_type=IPTypes.PUBLIC,
-        )
+        # The connector needs to refresh the SA token every now and then.
+        # The refresh is not thread-safe and leads to malformed SSL exchanges.
+        with lock:
+            conn: pg8000.dbapi.Connection = connector.connect(
+                instance_connection_name,
+                "pg8000",
+                user=iam_service_account_user,
+                db=database_name,
+                ip_type=IPTypes.PUBLIC,
+            )
         return conn
 
     engine = sqlalchemy.create_engine(
