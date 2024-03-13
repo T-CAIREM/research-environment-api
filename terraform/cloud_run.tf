86804d570bed0a5e90b881dfd6057058ca08aa9f
matkaczmarek <mat_kaczmarek@outlook.com>
2024-03-13 15:31:16 +0100

    Add db migration cloud job and load dynamically envs from array
    [T-CAIREM 1151]

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -4,6 +4,38 @@ locals {
     { name = "core", command = null, cpu_throttling = true },
     { name = "celery", command = ["/celery_endpoint.sh"], cpu_throttling = false }
   ]
+
+  env_vars = {
+    APP_ENV = "production"
+    C_FORCE_ROOT = "True"
+    SERVICE_ACCOUNT_CREDENTIALS_PATH = "/app/core_service_account_secret/${var.service_account_credentials_secret_name}"
+    CLOUD_BUILD_SERVICE_ACCOUNT_NAME = var.cloud_build_service_account_name
+    PROJECT_ID = var.project_id
+    DATABASE_NAME = var.database_name
+    DATABASE_USER = var.service_account_name
+    CLOUD_SQL_INSTANCE_CONNECTION_NAME = var.cloud_sql_instance_connection_name
+    CELERY_RESULT_BACKEND = var.celery_backend_url
+    CELERY_BROKER_URL = var.celery_broker_url
+    CACHE_TYPE = var.cache_type
+    BILLING_ACCOUNT_CREATOR_GROUP_ID = var.billing_account_creator_group_id
+    VPC_SECURE_PERIMETER_NAME = var.vpc_secure_perimeter_name
+    TERRAFORM_REPO_NAME = var.terraform_repo_name
+    TERRAFORM_BRANCH_NAME = var.terraform_branch_name
+    JUPYTER_STARTUP_SCRIPT = var.jupyter_startup_script
+    RSTUDIO_IMAGE_URL = var.rstudio_image_url
+    DATA_PROJECT_NAME = var.data_project_name
+    RSTUDIO_STARTUP_SCRIPT = var.rstudio_startup_script
+    NETWORK_NAME = var.network_name
+    RSTUDIO_DNS_PROJECT = var.rstudio_dns_project
+    RSTUDIO_DNS_ZONE = var.rstudio_dns_zone
+    RSTUDIO_DOMAIN_NAME = var.rstudio_domain_name
+    RSTUDIO_SSL_PRIVATE_KEY = var.rstudio_ssl_private_key
+    RSTUDIO_SSL_CERTIFICATE = var.rstudio_ssl_certificate
+    SHARING_FOLDER_ID = var.sharing_folder_id
+    WORKBENCHES_PARENT_PROJECT_ID = var.workbenches_parent_project_id
+    GCP_SIGNED_URL_EXPIRATION_TIME = var.gcp_signed_url_expiration_time
+    GCP_CORS_ALLOWED_ORIGINS = var.gcp_cors_allowed_origins
+  }
 }
 
 resource "google_cloud_run_service" "api" {
@@ -42,149 +74,12 @@ resource "google_cloud_run_service" "api" {
         }
 
         # HACK: Use a different serializer. Pickle requires this to be set in order to work. Bad idea.
-        env {
-          name  = "C_FORCE_ROOT"
-          value = "True"
-        }
-
-        env {
-          name  = "APP_ENV"
-          value = "production"
-        }
-
-        env {
-          name  = "SERVICE_ACCOUNT_CREDENTIALS_PATH"
-          value = "/app/core_service_account_secret/${var.service_account_credentials_secret_name}"
-        }
-
-        env {
-          name  = "CLOUD_BUILD_SERVICE_ACCOUNT_NAME"
-          value = var.cloud_build_service_account_name
-        }
-
-        env {
-          name  = "PROJECT_ID"
-          value = var.project_id
-        }
-
-        env {
-          name  = "DATABASE_NAME"
-          value = var.database_name
-        }
-
-        env {
-          name  = "DATABASE_USER"
-          value = var.service_account_name
-        }
-
-        env {
-          name  = "CLOUD_SQL_INSTANCE_CONNECTION_NAME"
-          value = var.cloud_sql_instance_connection_name
-        }
-
-        env {
-          name  = "CELERY_RESULT_BACKEND"
-          value = var.celery_backend_url
-        }
-
-        env {
-          name  = "CELERY_BROKER_URL"
-          value = var.celery_broker_url
-        }
-
-        env {
-          name  = "CACHE_TYPE"
-          value = var.cache_type
-        }
-
-        env {
-          name  = "BILLING_ACCOUNT_CREATOR_GROUP_ID"
-          value = var.billing_account_creator_group_id
-        }
-
-        env {
-          name  = "VPC_SECURE_PERIMETER_NAME"
-          value = var.vpc_secure_perimeter_name
-        }
-
-        env {
-          name  = "TERRAFORM_REPO_NAME"
-          value = var.terraform_repo_name
-        }
-
-        env {
-          name  = "TERRAFORM_BRANCH_NAME"
-          value = var.terraform_branch_name
-        }
-
-        env {
-          name  = "JUPYTER_STARTUP_SCRIPT"
-          value = var.jupyter_startup_script
-        }
-
-        env {
-          name  = "RSTUDIO_IMAGE_URL"
-          value = var.rstudio_image_url
-        }
-
-        env {
-          name  = "DATA_PROJECT_NAME"
-          value = var.data_project_name
-        }
-
-        env {
-          name  = "RSTUDIO_STARTUP_SCRIPT"
-          value = var.rstudio_startup_script
-        }
-
-        env {
-          name  = "NETWORK_NAME"
-          value = var.network_name
-        }
-
-        env {
-          name  = "RSTUDIO_DNS_PROJECT"
-          value = var.rstudio_dns_project
-        }
-
-        env {
-          name  = "RSTUDIO_DNS_ZONE"
-          value = var.rstudio_dns_zone
-        }
-
-        env {
-          name  = "RSTUDIO_DOMAIN_NAME"
-          value = var.rstudio_domain_name
-        }
-
-        env {
-          name  = "RSTUDIO_SSL_PRIVATE_KEY"
-          value = var.rstudio_ssl_private_key
-        }
-
-        env {
-          name  = "RSTUDIO_SSL_CERTIFICATE"
-          value = var.rstudio_ssl_certificate
-        }
-
-        env {
-          name  = "SHARING_FOLDER_ID"
-          value = var.sharing_folder_id
-        }
-
-        env {
-          name  = "WORKBENCHES_PARENT_PROJECT_ID"
-          value = var.workbenches_parent_project_id
-        }
-
-        env {
-          name  = "GCP_SIGNED_URL_EXPIRATION_TIME"
-          value = var.gcp_signed_url_expiration_time
-        }
-
-        env {
-          name  = "GCP_CORS_ALLOWED_ORIGINS"
-          value = var.gcp_cors_allowed_origins
+        dynamic "env" {
+          for_each = local.env_vars
+          content {
+            name  = env.key
+            value = env.value
+          }
         }
       }
 
@@ -198,6 +93,44 @@ resource "google_cloud_run_service" "api" {
         "run.googleapis.com/vpc-access-connector" = google_vpc_access_connector.connector.name
         "run.googleapis.com/cpu-throttling"       = each.value.cpu_throttling
       }
+    }
+  }
+}
+
+resource "google_cloud_run_v2_job" "migrate" {
+  name     = "migrate"
+  location = "us-central1"
+
+  template {
+    template {
+      volumes {
+        name = local.service_account_credentials_volume_name
+
+        secret {
+          secret = var.service_account_credentials_secret_name
+        }
+      }
+
+      containers {
+        image   = "${var.image_repository}:${var.image_tag}"
+        command = ["/bin/sh"]
+        args = ["-c", "alembic upgrade head"]
+
+        volume_mounts {
+          name       = local.service_account_credentials_volume_name
+          mount_path = "/app/core_service_account_secret"
+        }
+
+        dynamic "env" {
+          for_each = local.env_vars
+          content {
+            name  = env.key
+            value = env.value
+          }
+        }
+      }
+
+      service_account = var.service_account_name
     }
   }
 }
