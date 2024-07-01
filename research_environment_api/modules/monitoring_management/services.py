3a08e98f96bdd36cc328bcd78ca623755bccce71
matkaczmarek <mat_kaczmarek@outlook.com>
2024-07-01 17:13:55 +0200

    Calculate total time for given timestamps
    [T-CAIREM 1213]

--- research_environment_api/modules/monitoring_management/services.py
+++ research_environment_api/modules/monitoring_management/services.py
@@ -1,4 +1,13 @@
+from datetime import datetime
+
 from research_environment_api.modules.monitoring_management import entities, monitoring
+
+intervals = (
+    ("Days", 86400),  # 60 * 60 * 24
+    ("Hours", 3600),  # 60 * 60
+    ("Minutes", 60),
+    ("Seconds", 1),
+)
 
 
 def list_workbench_monitoring_data_entries():
@@ -18,9 +27,52 @@ def list_workbench_monitoring_data_entries():
 
     serialized_workbench_monitoring_data_entries = [
         entities.WorkbenchMonitoringDataEntry.transform_workbench_monitoring_data(
-            key, timestamps
+            key, _calculate_total_time(monitoring_data_to_timestamps[key])
         )
-        for key, timestamps in monitoring_data_to_timestamps.items()
+        for key in monitoring_data_to_timestamps.keys()
     ]
 
     return serialized_workbench_monitoring_data_entries
+
+
+def _calculate_total_time(timestamps):
+    now_timestamp = datetime.now()
+
+    # array of tuples (datetime, bool) where bool means if it is beginning or end, True == beginning
+    points = [(start, True) for start, _ in timestamps] + [
+        (end if end is not None else now_timestamp, False) for _, end in timestamps
+    ]
+
+    points.sort()
+
+    total_time = 0
+    active_intervals = 0
+    interval_start = None
+
+    for timestamp, is_start in points:
+        if is_start:
+            if active_intervals == 0:
+                interval_start = timestamp
+            active_intervals += 1
+            continue
+
+        active_intervals -= 1
+        if active_intervals == 0:
+            total_time += (timestamp - interval_start).total_seconds()
+            interval_start = None
+
+    return _display_time(total_time)
+
+
+def _display_time(seconds):
+    result = []
+
+    for name, count in intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            value = int(value)
+            if value == 1:
+                name = name.rstrip("s")
+            result.append("{} {}".format(value, name))
+    return ", ".join(result)
