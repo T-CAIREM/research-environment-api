1e1c89afa16943bd2efa3d577a219685d74bf1f0
Rafal Kosla <rafalkosla101@gmail.com>
2025-11-05 16:06:29 +0100

    Transfer code to other repository

--- terraform/06-memorystore.tf
+++
@@ -1,5 +0,0 @@
-resource "google_redis_instance" "redis" {
-  name               = "${var.name}-${terraform.workspace}-redis"
-  memory_size_gb     = 1
-  authorized_network = "projects/${var.project_id}/global/networks/${var.name}-network"
-}
