748e001a60e68f2ea8ecd2621aaf1a76f6412db1
Rafal Kosla <rafalkosla101@gmail.com>
2026-01-26 15:11:25 +0100

    Update google version for cluster

--- terraform/cluster/01-main.tf
+++ terraform/cluster/01-main.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.17"
+      version = "~> 7.15"
     }
     null = {
       source = "hashicorp/null"
